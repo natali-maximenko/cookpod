4ad0ecdd1f0db672311f857a2dc510027ef69632
Natali Maximenko <natali.maximenko@gmail.com>
2020-04-09 15:21:12 +0300

    Add auth, move rages to protected

--- lib/cookpod_web/router.ex
+++ lib/cookpod_web/router.ex
@@ -13,10 +13,22 @@ defmodule CookpodWeb.Router do
     plug :accepts, ["json"]
   end
 
+  pipeline :auth do
+    plug CookpodWeb.AuthPlug
+  end
+
   scope "/", CookpodWeb do
     pipe_through :browser
 
+    resources "/sessions", SessionController
+  end
+
+   # Definitely logged in scope
+   scope "/", CookpodWeb do
+    pipe_through [:browser, :auth]
+
     get "/", PageController, :index
+    get "/terms", PageController, :terms
   end
 
   # Other scopes may use custom stacks.
