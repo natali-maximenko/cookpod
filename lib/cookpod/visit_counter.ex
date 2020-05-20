868af549cf2eae970bf31b27416550765e002d39
Natali Maximenko <natali.maximenko@gmail.com>
2020-05-20 10:38:06 +0300

    Save visits in ETS

--- lib/cookpod/visit_counter.ex
+++ lib/cookpod/visit_counter.ex
@@ -1,5 +1,6 @@
 defmodule Cookpod.VisitCounter do
   use GenServer
+  @storage_name :cookpod_visits
 
   # Client
 
@@ -18,26 +19,43 @@ defmodule Cookpod.VisitCounter do
   # Server (callbacks)
   @impl true
   def init(data) do
+    :ets.new(@storage_name, [:named_table, :set, :public, read_concurrency: true])
     {:ok, data}
   end
 
   @impl true
   def handle_call(:statistic, _from, state) do
-    {:reply, state, state}
+    statistics = get_all() |> Enum.into(%{})
+    {:reply, statistics, state}
   end
 
   @impl true
-  def handle_cast({:visit, reciple_id}, state) do
-    {:noreply, update_state(state, reciple_id)}
+  def handle_cast({:visit, reciple_id}, _state) do
+    # {:noreply, update_state(state, reciple_id)}
+    {:noreply, update_storage(reciple_id)}
   end
 
-  defp update_state(state, reciple_id) do
-    case Map.fetch(state, reciple_id) do
-      {:ok, visits} ->
-        Map.merge(state, %{reciple_id => visits + 1})
+  defp get_all, do: :ets.tab2list(@storage_name)
 
-      :error ->
-        Map.merge(state, %{reciple_id => 1})
+  defp update_storage(reciple_id) do
+    case :ets.lookup(@storage_name, reciple_id) do
+      [] ->
+        :ets.insert(@storage_name, {reciple_id, 1})
+        {:ok, 1}
+
+      [{^reciple_id, visits}] ->
+        :ets.insert(@storage_name, {reciple_id, visits + 1})
+        {:ok, visits}
     end
   end
+
+  # defp update_state(state, reciple_id) do
+  #   case Map.fetch(state, reciple_id) do
+  #     {:ok, visits} ->
+  #       Map.merge(state, %{reciple_id => visits + 1})
+
+  #     :error ->
+  #       Map.merge(state, %{reciple_id => 1})
+  #   end
+  # end
 end
